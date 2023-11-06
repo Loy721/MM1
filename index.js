54d7d7947368d63e53be893d09a79f775c5196f5
Долгих Данил Юрьевич <danil.dolgikh@simbirsoft.com>
2023-11-06 21:53:54 +0300

    Shemas is done

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ class Planet {
     Vy;
     Ax;
     Ay;
+    lastAx;
+    lastAy;
     mass;
     radius;
     firstStep = true;
@@ -26,6 +28,32 @@ class Planet {
         this.Ay = 0;
         this.mass = mass;
         this.radius = radius;
+    }
+
+    setSpeed([x, y]){
+        this.lastX = this.x;
+        this.lastY = this.y;
+
+        this.x = x;
+        this.y = y;
+    }
+
+    calaculateAccelerationXY(planets) {
+        this.lastAx = this.Ax
+        this.lastAy = this.Ay;
+
+        let ax=0;
+        let ay=0;
+        for(let i = 0; i < planets.length; ++i) {
+            if (this != planets[i]) {
+                let planet = planets[i];
+                ax += Model.G * (planet.mass*(this.x - planet.x)) / 
+                        Math.pow( Math.sqrt( Math.pow(this.x-planet.x,2)+Math.pow(this.y-planet.y,2) ), 3);
+                ay += Model.G * (planet.mass*(this.y - planet.y)) / 
+                        Math.pow( Math.sqrt( Math.pow(this.x-planet.x,2)+Math.pow(this.y-planet.y,2) ), 3);
+            }
+        }
+        [this.Ax, this.Ay] = [-ax, -ay];
     }
 }
 
@@ -46,32 +74,14 @@ class Model {
 
     updateCoordinatePlanets() {
         for(let i = 0; i < this.planets.length; ++i) {
-            [this.planets[i].Ax, this.planets[i].Ay] = this.calaculateAccelerationXY(i);        
+            planets[i].calaculateAccelerationXY(this.planets);        
             
             this.defferentScheme.setVarsForNextStep(this.planets[i]);
-            let [xhlp, yhlp] = [planets[i].x, planets[i].y];
-            [planets[i].x, planets[i].y] = this.defferentScheme.calculateCoordinate();
-            [planets[i].lastX, planets[i].lastY] = [xhlp, yhlp];
+            this.planets[i].setSpeed(this.defferentScheme.calculateCoordinate());
 
             [planets[i].Vx, planets[i].Vy] = this.defferentScheme.calculateSpeed();
         }
     }    
-
-    calaculateAccelerationXY(indexCurrplanet) {//поменять не на индекс
-        let ax=0;
-        let ay=0;
-        for(let i = 0; i < this.planets.length; ++i) {
-            if (indexCurrplanet != i) {
-                let planet = this.planets[i];
-                let currPlanet = this.planets[indexCurrplanet];
-                ax += Model.G * (planet.mass*(currPlanet.x - planet.x)) / 
-                        Math.pow( Math.sqrt( Math.pow(currPlanet.x-planet.x,2)+Math.pow(currPlanet.y-planet.y,2) ), 3);
-                ay += Model.G * (planet.mass*(currPlanet.y - planet.y)) / 
-                        Math.pow( Math.sqrt( Math.pow(currPlanet.x-planet.x,2)+Math.pow(currPlanet.y-planet.y,2) ), 3);
-            }
-        }
-        return [-ax, -ay];
-    }
 }
 
 class Scheme {
@@ -110,11 +120,6 @@ class EilerKramerScheme extends Scheme {
  }
 
  class VerleScheme extends Scheme {
-
-    setVarsForNextStep(planet){
-        super.setVarsForNextStep(planet);
-    }
-
     calculateCoordinate() {
         if(this.currPlanet.firstStep) {
             this.currPlanet.firstStep = false;
@@ -134,45 +139,28 @@ class EilerKramerScheme extends Scheme {
  }
 
  class BimanScheme extends Scheme {
-    lastAx;
-    lastAy;
-    nextAx;
-    nextAy;
-    sheme;
-    firstStep = [true, true, true];
-
-    setVarsForNextStep(indexCurrplanet, planets){
-            this.lastAx = this.ax;
-            this.lastAy = this.ay;
-        super.setVarsForNextStep(indexCurrplanet, planets);
-    }
 
     calculateCoordinate() {
-        
-        if(this.firstStep[this.indexCurrplanet]){
-            this.sheme = new VerleScheme(this.dt);
-            this.sheme.setVarsForNextStep(this.indexCurrplanet, this.planets);
-            return this.sheme.calculateCoordinate();
+        if(this.currPlanet.firstStep) {
+            let eiler = new EilerScheme(this.dt);
+            eiler.setVarsForNextStep(this.currPlanet);
+            return eiler.calculateCoordinate()
         }
-        return [this.currPlanet.x + this.currPlanet.Vx * this.dt - (4 * this.ax - this.lastAx) * (this.dt * this.dt ) / 6, 
-                        this.currPlanet.y + this.currPlanet.Vy * this.dt - (4 * this.ay - this.lastAy) * (this.dt * this.dt ) / 6,];
+        return [this.currPlanet.x + this.currPlanet.Vx * this.dt - (4 * this.currPlanet.Ax - this.currPlanet.lastAx) * (this.dt * this.dt ) / 6, 
+                        this.currPlanet.y + this.currPlanet.Vy * this.dt - (4 * this.currPlanet.Ay - this.currPlanet.lastAy) * (this.dt * this.dt ) / 6];
     }
 
     calculateSpeed() {//TODO: очень плохо изменять lastX т.к. корректная работа класса зависит от последовательности вызова методов
-        if(this.firstStep[this.indexCurrplanet]){
-            this.firstStep[this.indexCurrplanet] = false;
-            return this.sheme.calculateSpeed();
+        if(this.currPlanet.firstStep) {
+            this.currPlanet.firstStep = false;
+            let eiler = new EilerScheme(this.dt);
+            eiler.setVarsForNextStep(this.currPlanet);
+            return eiler.calculateSpeed();
         }
-        [this.nextAx, this.nextAy] = this.calaculateAccelerationXY();
-
-        console.log(this.lastAx);
-        console.log(this.ax);
-        console.log(this.nextAx);
-        console.log("--------"+this.indexCurrplanet);
-
-        
-        return [this.currPlanet.Vx + (2 * this.nextAx + 5 * this.ax - this.lastAx) * this.dt / 6
-                        , this.currPlanet.Vy + (2 * this.nextAy + 5 * this.ay - this.lastAy) * this.dt / 6]
+        let [lastAx, lastAy] = [this.currPlanet.lastAx, this.currPlanet.lastAy];
+        this.currPlanet.calaculateAccelerationXY(model.planets);//TODO!!!!!!!!!!!!
+        return [this.currPlanet.Vx + (2 * this.currPlanet.Ax + 5 * this.currPlanet.lastAx - lastAx) * this.dt / 6,
+                        this.currPlanet.Vy + (2 * this.currPlanet.Ay + 5 * this.currPlanet.lastAy - lastAy) * this.dt / 6]
     }
  }
 
@@ -184,7 +172,7 @@ planet2 = new Planet(1.496 * Math.pow(10, 11), 0, 0, 29782, 5.9722 * Math.pow(10
 planet3 = new Planet(57.91 * Math.pow(10, 9), 0, 0, 47400, 3.33 * Math.pow(10,23), 1)
 planets = [planet1, planet2, planet3];
 let dt = 10000
-model = new Model(31536000, dt, planets, new VerleScheme(dt));
+model = new Model(31536000, dt, planets, new BimanScheme(dt));
 
 requestAnimationFrame(tick)
 
